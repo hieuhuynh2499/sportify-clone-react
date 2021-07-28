34203faf5addfaa1ad92669dae7d90d46d461063
unknown <hieuh5355@gmail.com>
2021-07-28 07:55:26 +0700

    fix 71%

--- src/containers/home/components/MusicDay.js
+++ src/containers/home/components/MusicDay.js
@@ -1,19 +1,22 @@
 import React from 'react'
 import './style.scss'
 import {FaPlay} from "react-icons/fa"
+import { Link } from 'react-router-dom'
 
 
 function MusicDay({music}) {
     return (
-        <div className="music-day">
-            <img className="music-day__img" src={music.image} alt="" />
-            <div className="music-day__content">
-               <p className="music-day__text"> {music.name}</p>
-               <div className="music-day__icon">
-                    <FaPlay/>
-               </div>
+       <Link to="/play-list">
+            <div className="music-day">
+                <img className="music-day__img" src={music.image} alt="" />
+                <div className="music-day__content">
+                <p className="music-day__text"> {music.name}</p>
+                <div className="music-day__icon">
+                        <FaPlay/>
+                </div>
+                </div>
             </div>
-        </div>
+       </Link>
     )
 }
 
